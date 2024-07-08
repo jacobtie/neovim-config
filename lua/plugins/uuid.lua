e4be0daf679d53bb5689f40196125036d7ade7c2
Jacob Krevat <jakrevat@redventures.com>
2024-07-08 08:35:22 -0400

    Fixing UUID insert placement

--- lua/plugins/uuid.lua
+++ lua/plugins/uuid.lua
@@ -4,6 +4,7 @@ return {
     require('uuid-nvim').setup {
       case = 'lower',
       quotes = 'none',
+      after = false,
     }
     vim.keymap.set('i', '<C-u>', require('uuid-nvim').insert_v4)
   end,
