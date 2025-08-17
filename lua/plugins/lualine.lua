7c89a46a5ef8fb23a35c4da64724b22fa79b9757
Jacob Krevat <jakrevat@redventures.com>
2025-08-17 13:56:13 -0400

    Tweaking lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -25,9 +25,9 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { { 'filename', path = 1 } },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
+        lualine_x = { 'encoding', 'fileformat' },
+        lualine_y = { 'filetype', 'lsp_status' },
+        lualine_z = { 'progress', 'location' },
       },
       inactive_sections = {
         lualine_a = {},
