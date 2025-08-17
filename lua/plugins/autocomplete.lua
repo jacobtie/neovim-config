217973da8afdba92799e55d26a131d7ea7a53db0
Jacob Krevat <jakrevat@redventures.com>
2025-08-17 14:07:25 -0400

    Moving windsurf to autocomplete.lua

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,90 +1,107 @@
 return {
-  'saghen/blink.cmp',
-  event = 'VimEnter',
-  version = '1.*',
-  dependencies = {
-    -- Snippet Engine
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
+  {
+    'saghen/blink.cmp',
+    event = 'VimEnter',
+    version = '1.*',
+    dependencies = {
+      -- Snippet Engine
+      {
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
+        opts = {},
       },
-      opts = {},
+      'folke/lazydev.nvim',
     },
-    'folke/lazydev.nvim',
-  },
-  --- @module 'blink.cmp'
-  --- @type blink.cmp.Config
-  opts = {
-    keymap = {
-      preset = 'default',
-    },
-    appearance = {
-      nerd_font_variant = 'mono',
-    },
-    completion = {
-      documentation = { auto_show = true },
-      -- brenoprata10/nvim-highlight-colors integration
-      menu = {
-        draw = {
-          components = {
-            -- customize the drawing of kind icons
-            kind_icon = {
-              text = function(ctx)
-                -- default kind icon
-                local icon = ctx.kind_icon
-                -- if LSP source, check for color derived from documentation
-                if ctx.item.source_name == 'LSP' then
-                  local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
-                  if color_item and color_item.abbr ~= '' then
-                    icon = color_item.abbr
+    --- @module 'blink.cmp'
+    --- @type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = 'default',
+      },
+      appearance = {
+        nerd_font_variant = 'mono',
+      },
+      completion = {
+        documentation = { auto_show = true },
+        -- brenoprata10/nvim-highlight-colors integration
+        menu = {
+          draw = {
+            components = {
+              -- customize the drawing of kind icons
+              kind_icon = {
+                text = function(ctx)
+                  -- default kind icon
+                  local icon = ctx.kind_icon
+                  -- if LSP source, check for color derived from documentation
+                  if ctx.item.source_name == 'LSP' then
+                    local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                    if color_item and color_item.abbr ~= '' then
+                      icon = color_item.abbr
+                    end
                   end
-                end
-                return icon .. ctx.icon_gap
-              end,
-              highlight = function(ctx)
-                -- default highlight group
-                local highlight = 'BlinkCmpKind' .. ctx.kind
-                -- if LSP source, check for color derived from documentation
-                if ctx.item.source_name == 'LSP' then
-                  local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
-                  if color_item and color_item.abbr_hl_group then
-                    highlight = color_item.abbr_hl_group
+                  return icon .. ctx.icon_gap
+                end,
+                highlight = function(ctx)
+                  -- default highlight group
+                  local highlight = 'BlinkCmpKind' .. ctx.kind
+                  -- if LSP source, check for color derived from documentation
+                  if ctx.item.source_name == 'LSP' then
+                    local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                    if color_item and color_item.abbr_hl_group then
+                      highlight = color_item.abbr_hl_group
+                    end
                   end
-                end
-                return highlight
-              end,
+                  return highlight
+                end,
+              },
             },
           },
         },
       },
-    },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev' },
-      providers = {
-        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        providers = {
+          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        },
       },
+      snippets = { preset = 'luasnip' },
+      fuzzy = { implementation = 'lua' },
+      signature = { enabled = true },
     },
-    snippets = { preset = 'luasnip' },
-    fuzzy = { implementation = 'lua' },
-    signature = { enabled = true },
+  },
+  {
+    'Exafunction/windsurf.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'hrsh7th/nvim-cmp',
+    },
+    config = function()
+      require('codeium').setup {
+        enable_cmp_source = false,
+        virtual_text = {
+          enabled = true,
+        },
+      }
+    end,
   },
 }
