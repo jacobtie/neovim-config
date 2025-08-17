3eb123acf1df85b4f983e639c99ac612c609b1e2
Jacob Krevat <jakrevat@redventures.com>
2025-08-17 11:50:58 -0400

    Setting up Snacks picker fully

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,9 +2,15 @@ return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
-  opts = {
-    bigfile = { enabled = true },
-    picker = { enabled = true },
-    quickfile = { enabled = true },
-  },
+  config = function()
+    local Snacks = require 'snacks'
+    Snacks.setup {
+      bigfile = { enabled = true },
+      picker = { enabled = true },
+      quickfile = { enabled = true },
+    }
+    vim.keymap.set('n', '<leader>sf', Snacks.picker.files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sg', Snacks.picker.grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sd', Snacks.picker.diagnostics, { desc = '[S]earch [D]iagnostics' })
+  end,
 }
